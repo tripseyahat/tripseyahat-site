d3f756d741d5167ca8951872f71fb00014cbda42
tripseyahat <info@orginos.com>
2025-08-30 20:46:15 +0300

    Create page.js

---
+++ app/hotels/page.js
@@ -0,0 +1,13 @@
+export default function HotelsPage() {
+  return (
+    <main className="container">
+      <h1>Hotels</h1>
+      <p>Find the best hotel deals for your trip.</p>
+      <ul>
+        <li>Hilton Istanbul - $120/night</li>
+        <li>Paris Marriott - $200/night</li>
+        <li>New York Plaza - $250/night</li>
+      </ul>
+    </main>
+  );
+}
