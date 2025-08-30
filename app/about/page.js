c8ff02a569f12eb8d31e2c02ee042391e2b42e08
tripseyahat <info@orginos.com>
2025-08-30 20:24:15 +0300

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,16 @@
+export default function AboutPage() {
+  return (
+    <main className="container">
+      <h1>Hakkımızda</h1>
+      <p>
+        Trip Seyahat, uygun fiyatlı uçuş ve seyahat fırsatlarını bulmana yardımcı
+        olan basit bir web sitesidir. Amacımız en hızlı şekilde karar vermeni sağlamaktır.
+      </p>
+      <ul>
+        <li>Hızlı ve anlaşılır arayüz</li>
+        <li>Uygun fiyat odaklı yaklaşım</li>
+        <li>Basit, reklamsız deneyim</li>
+      </ul>
+    </main>
+  );
+}
